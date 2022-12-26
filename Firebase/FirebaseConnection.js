aa386cea3513067c24bdec0d475f9971a76483df
DUXIS <38537077+Dustin2@users.noreply.github.com>
2022-12-26 14:00:05 -0600

    Update FirebaseConnection.js

--- Firebase/FirebaseConnection.js
+++ Firebase/FirebaseConnection.js
@@ -5,13 +5,13 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDWgX-sWcTfKJVuxq8zAXZT8Yj5nI1SHzU",
-    authDomain: "betafirebase-a9062.firebaseapp.com",
-    databaseURL: "https://betafirebase-a9062.firebaseio.com",
-    projectId: "betafirebase-a9062",
-    storageBucket: "betafirebase-a9062.appspot.com",
-    messagingSenderId: "204012253230",
-    appId: "1:204012253230:web:eb79c395b4cb8940f3afbf"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
